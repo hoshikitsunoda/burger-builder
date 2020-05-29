6864dfb5bfd877e1f9f25d7b43ac630fba403443
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-29 23:25:31 +0900

    added Aux and wrapped placeholder divs

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 
+import Aux from '../../hoc/Aux'
+
 class BurgerBuilder extends Component {
   render() {
-    return <div></div>
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    )
   }
 }
 
