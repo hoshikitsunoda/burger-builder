ee46281f5939d1965b7704f94a3f0f4ed041f383
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-31 00:27:36 +0900

    changed added prop to receive ctrl.type

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -18,7 +18,7 @@ const buildControls = (props) => {
           <BuildControl
             key={ctrl.label}
             label={ctrl.label}
-            added={props.ingredientAdded}
+            added={() => props.ingredientAdded(ctrl.type)}
           />
         )
       })}
