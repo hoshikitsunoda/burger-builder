e03e366b2771e51317f015f59efafe6987d1ddd4
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-31 00:19:38 +0900

    passed addIngredientHandler as prop to BuildControl

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,7 +39,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     )
   }
