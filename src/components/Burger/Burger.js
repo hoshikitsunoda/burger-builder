618f49744615e20e9ecf8280c8e6b3afae0e63ca
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-30 23:26:08 +0900

    added reduce to transformedIngredients logic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />
+      })
     })
-  })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
 
   return (
     <div className={classes.Burger}>
