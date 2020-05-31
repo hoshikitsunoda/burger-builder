67e990e7e9416da555968e190970e445d5c70e64
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-31 22:20:09 +0900

    passed ingredients to updatePurchaseState

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,10 +23,7 @@ class BurgerBuilder extends Component {
     purchasable: false,
   }
 
-  updatePurchaseState() {
-    const ingredients = {
-      ...this.state.ingredients,
-    }
+  updatePurchaseState(ingredients) {
     const sum = Object.keys(ingredients)
       .map((igKey) => {
         return ingredients[igKey]
@@ -49,7 +46,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice
     const newPrice = oldPrice + priceAddition
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-    this.updatePurchaseState()
+    this.updatePurchaseState(updatedIngredients)
   }
 
   removeIngredientHandler = (type) => {
@@ -66,7 +63,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice
     const newPrice = oldPrice - priceDeduction
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-    this.updatePurchaseState()
+    this.updatePurchaseState(updatedIngredients)
   }
 
   render() {
