2bb9588564e604228de433e43f7ab5390f37dfbe
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-02 01:54:32 +0900

    added memo, useEffect, useRef to replace shouldComponentUpdate

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { memo, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import classes from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
+const modal = memo(
+  (props) => {
+    return (
+      <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: props.show ? '1' : '0',
+          }}
+        >
+          {props.children}
+        </div>
+      </Aux>
+    )
+  },
+  (prevProps, nextProps) => prevProps.show === nextProps.show
 )
 
 modal.propTypes = {
