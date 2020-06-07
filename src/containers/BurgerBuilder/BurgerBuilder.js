7eecabc4389b49bda9667d4905386f5e2f36d0e0
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-08 01:43:40 +0900

    added logic to push price to queryParams and pass to Checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,29 +89,6 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // this.setState({ loading: true })
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Hoshki Tsunoda',
-    //     address: {
-    //       street: 'Test street',
-    //       zipCode: '99999',
-    //       country: 'Japan',
-    //     },
-    //     email: 'test@test.com',
-    //   },
-    //   deliveryMethod: 'fastest',
-    // }
-    // axios
-    //   .post('/orders.json', order)
-    //   .then((response) => {
-    //     this.setState({ loading: false, purchasing: false })
-    //   })
-    //   .catch((error) => {
-    //     this.setState({ loading: false, purchasing: false })
-    //   })
     const queryParams = []
     for (let i in this.state.ingredients) {
       queryParams.push(
@@ -120,6 +97,7 @@ class BurgerBuilder extends Component {
           encodeURIComponent(this.state.ingredients[i])
       )
     }
+    queryParams.push('price=' + this.state.totalPrice)
     const queryString = queryParams.join('&')
     this.props.history.push({
       pathname: '/checkout',
