50152b7430ef4d1b2dcf40c6ebcb48988de903fd
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-07 23:11:57 +0900

    added withRouter as an example to get router props

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  console.log(props)
   let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
@@ -32,4 +34,4 @@ burger.propTypes = {
   ingredients: PropTypes.object,
 }
 
-export default burger
+export default withRouter(burger)
