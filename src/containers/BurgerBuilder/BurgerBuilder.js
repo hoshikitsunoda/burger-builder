5970bdf94599dfdd977996b802ae6b6807df9e22
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-12 00:47:16 +0900

    first method to update UI

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,8 +13,6 @@ import * as actionTypes from '../../store/actions'
 
 class BurgerBuilder extends Component {
   state = {
-    totalPrice: 4,
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -40,7 +38,7 @@ class BurgerBuilder extends Component {
         return sum + el
       }, 0)
 
-    this.setState({ purchasable: sum > 0 })
+    return sum > 0
   }
 
   purchaseHandler = () => {
@@ -91,7 +89,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
