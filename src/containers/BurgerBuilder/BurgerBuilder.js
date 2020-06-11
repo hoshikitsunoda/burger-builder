ebf2e2cc248cc83f393d806418fbaead0511e5ea
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-12 00:11:59 +0900

    commented out componentDidMount code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,14 +27,14 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get('https://burger-builder-fc801.firebaseio.com/Ingredients.json')
-      .then((response) => {
-        this.setState({ ingredients: response.data })
-      })
-      .catch((error) => {
-        this.setState({ error: true })
-      })
+    // axios
+    //   .get('https://burger-builder-fc801.firebaseio.com/Ingredients.json')
+    //   .then((response) => {
+    //     this.setState({ ingredients: response.data })
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ error: true })
+    //   })
   }
 
   updatePurchaseState(ingredients) {
