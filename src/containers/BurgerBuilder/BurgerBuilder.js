49e6308e4ed99e5e3c1fe8e3b515c6732ffd6c21
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-13 00:25:26 +0900

    removed componentDidMount and moved fetching to actions file

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import axios from '../../axios-orders'
 
 import Aux from '../../hoc/Aux/Aux'
 import Burger from '../../components/Burger/Burger'
@@ -8,25 +9,11 @@ import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import Spinner from '../../components/UI/Spinner/Spinner'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
-import axios from '../../axios-orders'
 import * as burgerBuilderActions from '../../store/actions/index'
 
 class BurgerBuilder extends Component {
   state = {
     purchasing: false,
-    loading: false,
-    error: false,
-  }
-
-  componentDidMount() {
-    // axios
-    //   .get('https://burger-builder-fc801.firebaseio.com/Ingredients.json')
-    //   .then((response) => {
-    //     this.setState({ ingredients: response.data })
-    //   })
-    //   .catch((error) => {
-    //     this.setState({ error: true })
-    //   })
   }
 
   updatePurchaseState(ingredients) {
@@ -91,9 +78,6 @@ class BurgerBuilder extends Component {
           purchaseContinued={this.purchaseContinueHandler}
         />
       )
-    }
-    if (this.state.loading) {
-      orderSummary = <Spinner />
     }
 
     return (
