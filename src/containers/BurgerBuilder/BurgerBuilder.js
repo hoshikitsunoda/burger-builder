43e040cc6cd7c1b6abd6bf522e73d6e1c21f116f
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-13 00:36:52 +0900

    added onInit and error state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,6 +16,10 @@ class BurgerBuilder extends Component {
     purchasing: false,
   }
 
+  componentDidMount() {
+    this.props.onInitIngredients()
+  }
+
   updatePurchaseState(ingredients) {
     const sum = Object.keys(ingredients)
       .map((igKey) => {
@@ -49,7 +53,7 @@ class BurgerBuilder extends Component {
     }
     let orderSummary = null
 
-    let burger = this.state.error ? (
+    let burger = this.props.error ? (
       <p>Ingredients can't be loaded!</p>
     ) : (
       <Spinner />
@@ -99,6 +103,7 @@ const mapStateToProps = (state) => {
     // access ingredients state in this component
     ings: state.ingredients,
     price: state.totalPrice,
+    error: state.error,
   }
 }
 
@@ -108,6 +113,7 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(burgerBuilderActions.addIngredient(ingName)),
     onIngredientRemoved: (ingName) =>
       dispatch(burgerBuilderActions.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients()),
   }
 }
 
