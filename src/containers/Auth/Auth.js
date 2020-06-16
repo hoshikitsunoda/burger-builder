31c1e7304e372b375fc4f0e5219dfe928daab34d
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-17 01:35:03 +0900

    added authRedirect logic so the user is redirected to main page after login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router'
 
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
@@ -122,8 +123,14 @@ class Auth extends Component {
       errorMessage = <p>{this.props.error.message}</p>
     }
 
+    let authRedirect = null
+    if (this.props.isAuthenticated) {
+      authRedirect = <Redirect to="/" />
+    }
+
     return (
       <div className={classes.Auth}>
+        {authRedirect}
         {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
@@ -142,6 +149,7 @@ const mapStateToProps = (state) => {
     // because there's combined reducer, it needs to access state using state.auth.state
     loading: state.auth.loading,
     error: state.auth.error,
+    isAuthenticated: state.auth.token !== null,
   }
 }
 
