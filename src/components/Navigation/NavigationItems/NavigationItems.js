5806c0f2c739c7c7352cdbccad171f9c5f702057
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-16 23:34:23 +0900

    fixed NavItem render condition to follow the instruction

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,10 +9,10 @@ const navigationItems = (props) => (
       Burger Builder
     </NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
-    {props.isAuthenticated ? (
-      <NavigationItem link="/logout">Logout</NavigationItem>
-    ) : (
+    {!props.isAuthenticated ? (
       <NavigationItem link="/auth">Authenticate</NavigationItem>
+    ) : (
+      <NavigationItem link="/logout">Logout</NavigationItem>
     )}
   </ul>
 )
